c75c9a17dc5e778b5d8039d17d1f98558faecdf0
RhodyArthur <rhodaarthur48@gmail.com>
2026-01-07 17:28:24 +0000

    feat: add initial structure for models, services, storage, and validators

--- models/contact.py
+++ models/contact.py
@@ -1,9 +1,7 @@
+import json
 class Contact:
     def __init__(self, name, email, phone, address):
         self.name = name
         self.email = email
         self.phone = phone
         self.address = address
-
-class ContactBook(Contact):
-    pass
