dd49bcfc7ed2b02798b943f9d1a4b4b907d5db12
RhodyArthur <rhodaarthur48@gmail.com>
2026-01-07 18:29:47 +0000

    feat: add validation methods for contact fields

--- models/contact.py
+++ models/contact.py
@@ -1,7 +1,39 @@
-import json
+from ..validators.validators import validate_email, validate_name, validate_phone
+
 class Contact:
-    def __init__(self, name, email, phone, address):
+    def __init__(self, name: str, email: str, phone: str, address: str):
         self.name = name
         self.email = email
         self.phone = phone
         self.address = address
+        self.validate_fields()
+
+    def validate_name(self):
+        if not validate_name(self.name):
+            raise ValueError("Invalid name")
+        return self.name
+        
+        
+    def validate_email(self):
+        if not validate_email(self.email):
+            raise ValueError("Invalid email")
+        return self.email
+        
+        
+    def validate_phone(self):
+        if not validate_phone(self.phone):
+            raise ValueError("Invalid phone number")
+        return self.phone
+        
+        
+    def validate_address(self):
+        if not isinstance(self.address, str) or len(self.address) < 3:
+            raise ValueError("Invalid address")
+        return self.address
+    
+    def validate_fields(self):
+        self.validate_name()
+        self.validate_email()
+        self.validate_phone()
+        self.validate_address()
+        
