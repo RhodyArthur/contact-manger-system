4e420a1785b8cf184bd732ee20978c8bcd997651
RhodyArthur <rhodaarthur48@gmail.com>
2026-01-09 19:15:30 +0000

    feat: add methods for dictionary conversion and string representation

--- models/contact.py
+++ models/contact.py
@@ -7,3 +7,25 @@ class Contact:
         self.email = email
         self.phone = phone
         self.address = address
+
+    def to_dict(self):
+        """Convert Contact to dictionary for JSON"""
+        return {
+            "name": self.name,
+            "email": self.email,
+            "phone": self.email,
+            "address": self.address
+        }
+    
+    @classmethod
+    def from_dict(cls, data):
+        """Create Contact from dictionary"""
+        return cls(
+            name=data["name"],
+            email=data["email"],
+            phone=data["phone"],
+            address= data["address"])
+        
+    def __str__(self):
+        """Human-readable representation"""
+        return f'Name: {self.name}, Email: {self.email}, Phone: {self.phone}, Address: {self.address}'
